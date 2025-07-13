ee22849d0525c30c3d0a2b625a232e9476ec8f22
nilsmango <simonmilagrolang@icloud.com>
2025-07-13 09:21:02 +0200

    Updated some stuff.

---
+++ DeviceActivityMonitorExtension/DeviceActivityMonitorExtension.swift
@@ -0,0 +1,49 @@
+//
+//  DeviceActivityMonitorExtension.swift
+//  DeviceActivityMonitorExtension
+//
+//  Created by Simon Lang on 12.07.2025.
+//
+
+import DeviceActivity
+import Foundation
+import ManagedSettings
+
+// Optionally override any of the functions below.
+// Make sure that your class name matches the NSExtensionPrincipalClass in your Info.plist.
+class DeviceActivityMonitorExtension: DeviceActivityMonitor {
+    override func intervalDidStart(for activity: DeviceActivityName) {
+        super.intervalDidStart(for: activity)
+        
+    }
+    
+    override func intervalDidEnd(for activity: DeviceActivityName) {
+        super.intervalDidEnd(for: activity)
+        
+    }
+    
+    override func eventDidReachThreshold(_ event: DeviceActivityEvent.Name, activity: DeviceActivityName) {
+        super.eventDidReachThreshold(event, activity: activity)
+        
+        // Handle the event reaching its threshold.
+    }
+    
+    override func intervalWillStartWarning(for activity: DeviceActivityName) {
+        super.intervalWillStartWarning(for: activity)
+        
+        // Handle the warning before the interval starts.
+    }
+    
+    override func intervalWillEndWarning(for activity: DeviceActivityName) {
+        super.intervalWillEndWarning(for: activity)
+        
+        // Handle the warning before the interval ends.
+    }
+    
+    override func eventWillReachThresholdWarning(_ event: DeviceActivityEvent.Name, activity: DeviceActivityName) {
+        super.eventWillReachThresholdWarning(event, activity: activity)
+        
+        // Handle the warning before the event reaches its threshold.
+    }
+    
+}
